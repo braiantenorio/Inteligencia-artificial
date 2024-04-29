4ddcd09a7d99d9924ecbb13a43fd63a6d88d09bd
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-04-29 17:55:24 -0300

    punto14

--- TP2/punto14.pl
+++ TP2/punto14.pl
@@ -4,25 +4,6 @@
 :- op(100, xfx, con).
 
 
-miembro(X, [X|Y]).
-miembro(X, [Y|Z]) :- miembro(X,Z).
-
-%concatenar([],L2,L2).
-%concatenar(L1,[],L1).
-%concatenar([X|R], L, [X|Z]) :- concatenar(R,L,Z).
-
-sublista(Sublista, Lista) :-
-    sufijo(Sufijo, Lista),
-    prefijo(Sublista, Sufijo).
-
-prefijo([], []).
-prefijo([X|Xs], [X|Prefijo]) :-
-    prefijo(Xs, Prefijo).
-
-sufijo(Sufijo, Lista) :-
-    append(_, Sufijo, Lista).
-
-
 X en [X|_].
 X en [_|Y] :- X en Y.
 
