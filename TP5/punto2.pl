aaab982de65ccdaefdf9235fa38d804da98cc487
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-05-21 14:44:12 -0300

    tp5 terminado

--- TP5/punto2.pl
+++ TP5/punto2.pl
@@ -1,19 +1,23 @@
-o --> fn(_,_),fv(_).
+o --> fn(_,N),fv(N).
 fn(G,N) --> art(G,N),sust(G,N).
-fv(N) --> verbo(N),fp(_,N).
+fv(N) --> verbo(N),fp(_,_).
 fp(G,N) --> det(G,N),sust(G,N).
 
 sust(fem,plu) --> [casas];[mesas];[profesoras].
+sust(fem,sing) --> [profesora];[gata];[casa].
 sust(masc,sing) --> [gato];[libro];[disco];[mundo];[profesor].
-sust(masc,plu) --> [profesores].
+sust(masc,plu) --> [profesores];[discos].
 
-verbo(sing) --> [lee];[corre];[escribe];[toma].
-verbo(plu) --> [leen];[corren];[escriben];[toman].
+verbo(sing) --> [lee];[corre];[escribe].
+verbo(plu) --> [leen];[corren];[escriben].
 
-art(masc,plu) --> [los];[unos].
-art(fem,sing) --> [la];[una].
+art(masc,plu) --> [los].
+art(masc,sing) --> [el].
+art(fem,sing) --> [la].
+art(fem,plu) --> [las].
 
 det(masc,sing) --> [un].
 det(masc,plu) --> [unos].
 det(fem,sing) --> [una].
+det(fem,plu) --> [unas].
 
